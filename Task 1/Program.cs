44fbe7063e99fc19760356a21d02ad6338ef18fa
Danil Neklyudov <dejeksdan@gmail.com>
2023-02-20 23:06:00 +0500

    Initial commit

---
+++ Task 1/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Задача 10 Напишите программу, которая принимает на вход 
+трёхзначное число и на выходе 
+показывает вторую цифру этого числа.*/
+
+int numb = 0;
+
+while(true)
+{
+    numb = Convert.ToInt32(Console.ReadLine());
+    if (numb % 1000 == numb & numb % 100 != numb) // Условие 3х значности для int
+    {
+        numb /= 10;
+        Console.WriteLine(numb % 10);
+    }
+    else{Console.WriteLine("Введите 3х значное число!");}
+}
