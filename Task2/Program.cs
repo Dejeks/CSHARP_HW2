42bed72af8dcb5087885f0c1bb23def7a4353396
Danil Neklyudov <dejeksdan@gmail.com>
2023-02-20 23:21:35 +0500

    Problem 13 solved

---
+++ Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Задача 13:
+Напишите программу, которая выводит третью цифру 
+заданного числа или сообщает, что третьей цифры нет. */
+
+int result = -1;
+int number = 0;
+
+while(true)
+{
+    number = Convert.ToInt32(Console.ReadLine());
+    if (number >= 100)
+    {
+        while (number > 999)
+        {
+            number = number / 10;
+        }
+        Console.WriteLine(number % 10);
+    }
+}
